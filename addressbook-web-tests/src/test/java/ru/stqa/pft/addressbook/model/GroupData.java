738a6024c83d23d7f45aaf4aed03ae9cf1d9943c
EkaterinaShvets <68472226+EkaterinaShvets@users.noreply.github.com>
2020-08-25 23:45:27 +0300

    Использование гладких интерфесов вместо контруктора

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,16 +4,29 @@ import java.util.Objects;
 
 public class GroupData {
 
-  private int id;
-  private final String name;
-  private final String header;
-  private final String footer;
+  private int id = Integer.MAX_VALUE;
+  private String name;
+  private String header;
+  private String footer;
 
-  public GroupData(String name, String header, String footer) {
-    this.id = Integer.MAX_VALUE;
+   public GroupData withId(int id) {
+    this.id = id;
+    return this;
+  }
+
+  public GroupData withName(String name) {
     this.name = name;
+    return this;
+  }
+
+  public GroupData withHeader(String header) {
     this.header = header;
+    return this;
+  }
+
+  public GroupData withFooter(String footer) {
     this.footer = footer;
+    return this;
   }
 
   @Override
@@ -28,18 +41,6 @@ public class GroupData {
   public int hashCode() {
     return Objects.hash(name);
   }
-
-  public GroupData(int id, String name, String header, String footer) {
-    this.id = id;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
 
   @Override
   public String toString() {
